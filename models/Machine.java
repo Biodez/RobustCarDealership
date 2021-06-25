d76945eddcd16a0c4e9d318f0a16c82862b674e8
Afeez <aoladokun@my.apsu.edu>
2021-06-24 22:59:29 -0500

    Building a robust car dealership project

---
+++ models/Machine.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Machine {
+    private Item[][] items;
+
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+    
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+    
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
+
+    public void dispense(int row, int spot) {
+        if(items[row][spot].getQuantity() == 0) {
+            throw new IllegalArgumentException("Quantity cannot be less than zero");
+        }
+        items[row][spot].setQuantity(items[row][spot].getQuantity() - 1);
+    }
+
+    public int getLength() {
+        return this.items.length;
+    }
+
+    public int getRowLength() {
+        return this.items[0].length;
+    }
+
+    public boolean isEmpty(int row, int spot) {
+        if (this.items[row][spot].getQuantity() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString() + " ";
+            }
+            temp += "\n\n";
+        }
+        temp += "\t************************************************";
+        return temp;
+    }
+
+}
